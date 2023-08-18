6f466ab6c21a30c167121ff12714c2e9c5024f4f
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2023-08-18 17:20:05 -0400

    CI
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

---
+++ .github/workflows/executor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Loop over all .ipynb files in the current directory and its subdirectories
+find . -type f -name '*.ipynb' | while read file; do
+    abs_path=$(realpath $file)
+    dir=$(dirname $file)
+    abs_dir=$(realpath $dir)
+
+    # Change the working directory to the file's directory
+    (cd $abs_dir && jupyter nbconvert --to notebook --execute $abs_path)
+done
