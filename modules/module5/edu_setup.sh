09404ffcec978fe59d89b9a2d5c3d160f142b779
Chaichontat Sriworarat <csriwor1@jh.edu>
2023-09-29 20:27:53 -0400

    install packages manually
    Signed-off-by: Chaichontat Sriworarat <csriwor1@jh.edu>

--- modules/module5/edu_setup.sh
+++ modules/module5/edu_setup.sh
@@ -7,10 +7,6 @@ bash Miniforge3-Linux-x86_64.sh -b
 miniforge3/condabin/conda init
 rm Miniforge3-Linux-x86_64.sh
 
-source ~/.bashrc
-mamba install notebook matplotlib plotnine pandas
-pip install pyfastx
-
 curl -LSsO https://update.code.visualstudio.com/1.75.1/linux-x64/stable
 tar xvzf stable
 rm stable
